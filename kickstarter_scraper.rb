2c4178f36e233f723b15455d0cd4a11dba87a75c
Jesse Griffin <jesse.griffin@gmail.com>
2020-03-01 00:54:24 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,6 +2,8 @@
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
+# location: project.css("ul.project-meta span.location-name").text
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 
 require 'nokogiri'
 require 'pry' 
@@ -9,7 +11,13 @@ require 'pry'
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  binding.pry 
+  projects = {}
+  
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {}
+  end 
+  projects 
 end
 
 create_project_hash 
